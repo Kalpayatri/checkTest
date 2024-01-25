b17c1725288f693669af115f09ac27ba4a1f9e67
Your Name <khusbu.mallick@gmail.com>
2024-01-25 11:14:05 +0530

    Posts Collection

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,18 +3,20 @@ import path from 'path'
 import { payloadCloud } from '@payloadcms/plugin-cloud'
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { webpackBundler } from '@payloadcms/bundler-webpack'
-import { slateEditor } from '@payloadcms/richtext-slate'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import { buildConfig } from 'payload/config'
 
 import Users from './collections/Users'
+import Posts from './collections/Posts'
+
 
 export default buildConfig({
   admin: {
     user: Users.slug,
     bundler: webpackBundler(),
   },
-  editor: slateEditor({}),
-  collections: [Users],
+  editor: lexicalEditor({}),
+  collections: [Users,Posts],
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
   },
